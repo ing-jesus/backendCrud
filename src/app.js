ada1cece67aef93987c53d66ed22ffb8262b6c9a
ing-jesus <jesus.pineda0311@gmail.com>
2024-12-08 23:26:30 -0500

    Actualización de lógica en src/app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 import authRoutes from "./routes/usersRoutes.js";
 import taskRouter from "./routes/tasksRoute.js";
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
